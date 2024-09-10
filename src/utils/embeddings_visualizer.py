d6756cb1f64bdce2f1eae0bc992a82d48d51cc00
anant996 <anant.chaudhary@dataverze.ai>
2024-09-11 00:06:17 +0530

    removed unnecessary imports

--- src/utils/embeddings_visualizer.py
+++ src/utils/embeddings_visualizer.py
@@ -1,13 +1,4 @@
 from chromaviz import visualize_collection
-
-__import__('pysqlite3')
-import sys
-
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-# connection = sqlite3.connect('cache.db', timeout=100)
-import os
-import sys
-
 from langchain_community.vectorstores import Chroma
 from src.utils.functions import  hf_embeddings
 
